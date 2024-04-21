b7a54c01ad0fc41619d02a6003fabcc4f407047f
Cracklitosss <marroquinca17@gmail.com>
2024-04-20 21:32:09 -0600

    Preparacion ServerWS

---
+++ build/server.js
@@ -0,0 +1,61 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const socket_io_1 = require("socket.io");
+const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const port = process.env.WEBSOCKET_PORT || '3004';
+const io = new socket_io_1.Server(parseInt(port), {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST", "PUT"]
+    }
+});
+// Almacenar datos de usuarios por socket ID
+const clientData = {};
+io.on("connection", (socket) => {
+    console.log(`Nuevo cliente conectado: ${socket.id}`);
+    socket.on("authenticate", (data) => {
+        const token = data.token;
+        if (token) {
+            try {
+                const decoded = jsonwebtoken_1.default.verify(token, process.env.JWT_SECRET || '12345');
+                if (typeof decoded === 'object' && 'id' in decoded && 'IdEsp' in decoded) {
+                    clientData[socket.id] = { userId: decoded.id, IdEsp: Number(decoded.IdEsp) };
+                    console.log(`Datos almacenados para este socket después de autenticar [Socket ID: ${socket.id}]:`, clientData[socket.id]);
+                    socket.emit("authenticated", { success: true, message: "Autenticación exitosa." });
+                }
+                else {
+                    throw new Error("Invalid token structure");
+                }
+            }
+            catch (error) {
+                console.error(`Error al verificar el token desde ${socket.id}:`, error);
+                socket.emit("auth_error", "Token inválido");
+            }
+        }
+        else {
+            console.log(`No se proporcionó token desde ${socket.id}`);
+            socket.emit("auth_error", "No se proporcionó token");
+        }
+    });
+    socket.on("sensorData", (sensorData) => {
+        console.log(`Datos de sensor recibidos en ${socket.id}:`, sensorData);
+        io.emit("updateSensorData", sensorData);
+        // Adicionalmente, enviar datos al cliente específico que coincida con IdEsp
+        Object.entries(clientData).forEach(([socketId, data]) => {
+            if (data.IdEsp === sensorData.IdEsp) {
+                console.log(`Enviando datos al cliente específico en socket ${socketId}`);
+                io.to(socketId).emit("updateSensorDataSpecific", sensorData);
+            }
+        });
+    });
+    socket.on("disconnect", () => {
+        console.log(`Cliente desconectado: ${socket.id}`);
+        delete clientData[socket.id];
+    });
+});
+console.log(`Servidor WebSocket escuchando en el puerto ${port}`);
